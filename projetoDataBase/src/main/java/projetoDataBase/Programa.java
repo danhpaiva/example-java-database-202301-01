33d1d558c51d771d7c5996db06576a1ab8486095
Paiva <danpaiva@live.com>
2023-06-16 14:41:28 -0300

    feat: mudanças pontuais

--- projetoDataBase/src/main/java/projetoDataBase/Programa.java
+++ projetoDataBase/src/main/java/projetoDataBase/Programa.java
@@ -5,16 +5,21 @@ import java.sql.*;
 public class Programa {
 	// Configurações do banco de dados
 	private static final String URL = "jdbc:mysql://localhost:3306/dbempresa";
-	private static final String USERNAME = "root";
+	private static final String USERNAME = "user";
 	private static final String PASSWORD = "12345";
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
+
+		System.out.println("Programa com Acesso ao Banco de Dados");
+
+		Connection conn = null;
+
 		try {
 			// Carregar o driver JDBC
 			Class.forName("com.mysql.cj.jdbc.Driver");
 
 			// Conectar ao banco de dados
-			Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 
 			// Executar uma consulta
 			String sql = "SELECT * FROM emp";
@@ -30,12 +35,14 @@ public class Programa {
 			// Fechar as conexões
 			resultSet.close();
 			statement.close();
-			conn.close();
 
 		} catch (Exception e) {
 			System.out.println("Erro ao tentar acessar o banco!");
+			System.out.println("Erro: " + e.getMessage());
 			e.printStackTrace();
 		} finally {
+			if (conn != null)
+				conn.close();
 			System.out.println("Operação concluída!");
 		}
 	}
